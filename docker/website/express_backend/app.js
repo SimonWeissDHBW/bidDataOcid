9a467bc560f8681c45c2a6ceef449a4b4efa58cb
Simon Weiss <simonw.sw94@gmail.com>
2022-11-29 17:25:22 +0100

    Removed dist from backend

--- docker/website/express_backend/app.js
+++ docker/website/express_backend/app.js
@@ -21,7 +21,7 @@ app.use('/cellTowers', cellTowersRouter);
 app.use(express.static(process.cwd() + '/dist'));
 
 app.get('/', (req,res) => {
-  res.sendFile(process.cwd() + './dist/index.html');
+  res.sendFile('./dist/index.html');
 });
 
 app.listen(port, () => {
