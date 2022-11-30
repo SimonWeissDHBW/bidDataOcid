18924077db1d8d77d33b4c94bca4c53ea38c6b93
Simon Weiss <simonw.sw94@gmail.com>
2022-11-30 16:25:42 +0100

    Adjusted SQL Statement

--- docker/website/express_backend/routes/cellTowers.js
+++ docker/website/express_backend/routes/cellTowers.js
@@ -6,9 +6,9 @@ router.get('/:radio/:lonParam/:latParam', function(req, res) {
   // console.log("Longitude: " + req.params.lonParam + "\nLatitude: " + req.params.latParam);
   // let sql = `SELECT * FROM towers WHERE radio = '${req.params.radio}' AND st_distance_Sphere(point(lon, lat), point(${req.params.lonParam}, ${req.params.latParam})) <= towers.range LIMIT 1`;
   let sql = `SELECT MIN(st_distance_Sphere(point(TowersInRange.lon, TowersInRange.lat), point(${req.params.lonParam}, ${req.params.latParam}))) AS distance, TowersInRange.radio FROM 
-	(SELECT radio, lon, lat  FROM towers WHERE radio = '${req.params.radio}' AND st_distance_Sphere(point(lon, lat), point(${req.params.lonParam}, ${req.params.latParam})) <= towers.range) TowersInRange
+	(SELECT radio, lon, lat  FROM towers_${req.params.radio} WHERE st_distance_Sphere(point(lon, lat), point(${req.params.lonParam}, ${req.params.latParam})) <= towers.range) TowersInRange
   GROUP BY radio`;
-  
+
   console.log(sql)
   queryDB(sql, res);
 });
