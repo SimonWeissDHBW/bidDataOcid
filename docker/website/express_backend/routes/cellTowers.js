78df21853c4bb2839caba703b7e82c96d2f91de7
Simon Weiss <simonw.sw94@gmail.com>
2022-12-01 10:36:06 +0100

    Made backend async (hopefully)

--- docker/website/express_backend/routes/cellTowers.js
+++ docker/website/express_backend/routes/cellTowers.js
@@ -1,18 +1,18 @@
 const express = require('express'),
   router = express.Router();
 
-router.get('/:radio/:lonParam/:latParam', function(req, res) {
+router.get('/:radio/:lonParam/:latParam', async function(req, res) {
 
   let sql = `SELECT MIN(st_distance_Sphere(point(TowersInRange.lon, TowersInRange.lat), point(${req.params.lonParam}, ${req.params.latParam}))) AS distance, TowersInRange.radio FROM 
 	(SELECT radio, lon, lat  FROM towers_${req.params.radio} WHERE st_distance_Sphere(point(lon, lat), point(${req.params.lonParam}, ${req.params.latParam})) <= towers_${req.params.radio}.range) TowersInRange
   GROUP BY radio`;
 
   console.log(sql)
-  queryDB(sql, res);
+  await queryDB(sql, res);
 });
 
-function queryDB(sql, res) {
-  db.query(sql, function(err, data, fields) {
+async function queryDB(sql, res) {
+  await db.query(sql, function(err, data, fields) {
     if (err) throw err;
     console.log("Success");
     res.json({
