bdc828968eb5e01d7b82408911b2a1a7c26d9cc8
Simon Weiss <simonw.sw94@gmail.com>
2022-12-01 18:18:38 +0100

    Removed console log

--- docker/website/vue_frontend/src/components/MainPage.vue
+++ docker/website/vue_frontend/src/components/MainPage.vue
@@ -117,8 +117,6 @@ export default {
           .then(response => response.json())
             .then(data =>  {
 
-              console.log(data.data)
-
               this.options.find(x => x.value === this.group[i]).loading = false;
               let tempDistance = (data.data[0].length != 0) ? Math.round(data.data[0]) : "N/A";
               this.rows = this.rows.concat([{radio: this.group[i], distance: tempDistance}]);
